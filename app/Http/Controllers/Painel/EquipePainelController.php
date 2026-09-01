c0dc6d850eaffa7abff891136ccdbbe5c41193b6
leadcerto <leadecerto@gmail.com>
2026-09-01 16:17:14 -0300

    fix: importar DB facade em EquipePainelController

--- app/Http/Controllers/Painel/EquipePainelController.php
+++ app/Http/Controllers/Painel/EquipePainelController.php
@@ -10,6 +10,7 @@ use App\Services\MediaProcessorService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 
